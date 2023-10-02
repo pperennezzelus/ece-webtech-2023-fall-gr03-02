3760c5a9e63b1563871c40b29a6494d94cd12a4e
sebtrn <sebastien.tran@edu.ece.fr>
2023-10-02 21:09:39 +0200

    feat: add routes
    defining deleteRoutes, getRoutes, postRoutes and putRoutes
    
    Update dall the files
    
    Ajusted and Updated my files program with corrections made by the Teacher
    
    lab3 - initializing routes
    
    initializing express routes

--- lab2/handles.js
+++ lab2/handles.js
@@ -1,48 +1,59 @@
-// Necessary imports
+const url = require('url')
+const qs = require('querystring')
+
 module.exports = {
-    serverHandle: function (req, res) {
-        const url = require('url')
-        const qs = require('querystring')
-        const queryParams = qs.parse(url.parse(req.url).query)
-   
-        const route = url.parse(req.url)
-        const path = route.pathname 
-        const params = qs.parse(route.query)
-      
-      
-          console.log(path)
-          console.log(queryParams)
-      
+  serverHandle: function (req, res) {
+    const route = url.parse(req.url)
+    const path = route.pathname 
+    const params = qs.parse(route.query)
+    // Routes
+    switch (path) {
+      case '/':
         res.writeHead(200, {'Content-Type': 'text/html'})
-      
-      
-        //res.write(path)
-        if(path === '/')
-        {
-            res.writeHead(200, { 'Content-Type': 'text/plain' });
-            res.write('How this app work\nIf you put a name on the path of the application it will reply something to you \nhello takes a name query parameter and random names reply hello [name] (http://localhost:8080/hello?name=Jean)\nMy own name replies with a short intro of myself \nAny other path replies a 404 code with a not found message')
+        const content = '<!DOCTYPE html>' +
+        '<html>' +
+        '    <head>' +
+        '        <meta charset="utf-8" />' +
+        '        <title>Web Technologies project</title>' +
+        '    </head>' + 
+        '    <body>' +
+        '       <h1>Web Technologies project</h1>' +
+        '       <h2>Usage instructions</h2>' +
+        '       <ul>' +
+        '           <li>Go to the <a href="/hello">"Hello page"</a> to greet anonymous person.</li>' +
+        '           <li>Use the "<code>name</code>" query parameter to personalize the hello message. For example, <a href="/hello?name=Steve">greet Seb</a>.</li>' +
+        '           <li>Use "<code>?name=Seb</code>" to show an info about Seb. Go to the <a href="/hello?name=seb">Seb\'s page</a>.</li>' +
+        '           <li>Content pages: <a href="/about">About</a>, <a href="/contacts">Contacts</a></li>' +
+        '           <li>Other pages will respond with 404 error. For example, go to <a href="/random-page">this random page</a>.</li>' +
+        '       </ul>' +
+        '    </body>' +
+        '</html>'
+        res.write(content)
+        break
+      case '/hello':
+        res.writeHead(200, {'Content-Type': 'text/plain'})
+        if ('name' in params) {
+          if (params['name'].toLowerCase() === 'seb') {
+            res.write('Hello! I am Seb, I am a Web developer.')
+          } else {
+            res.write(`Hello ${params['name']}!`)
+          }
+        } else {
+          res.write('Hello anonymous!')
         }
-        else if (path === '/hello' && 'name' in params) {
-            if(params['name'] === 'Seb')
-            {
-                res.write('I m Seb the developper of this small application')
-
-            }else{
-                res.write('Hello '+ params['name']);
-            }
-
-        }else if(path === '/about'){
-
-            const data = require('./content/about.json')
-            res.writeHead(200, { 'Content-Type': 'application/json' })
-            res.write(JSON.stringify(data))
-            res.end()
+        break
+      default:
+        try {
+          const data = require(`./content/${path}.json`);
+          res.writeHead(200, {'Content-Type': 'application/json'})
+          res.write(JSON.stringify(data))
+        } catch (err) {
+          // If no file present, return 404 error
+          res.writeHead(404, {'Content-Type': 'text/plain'})
+          res.write("Error 404! The page doesn't exist.")
         }
-         else{
-            res.writeHead(404, { 'Content-Type': 'text/plain' });
-            res.write('Message not found (404)\n');
-        }
-        
-        res.end()
-    } 
+    }
+    // Finish the response
+    res.end()
   }
+}
---
+++ routes/getRoutes.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const router = express.Router();
+const db = require("../database");
+
+router.get("/hello/:name", (req, res) => {
+  res.send("Hello " + req.params.name);
+});
+
+router.get("/articles", (req, res) => {
+  res.json(db);
+});
+
+router.get("/articles/:articleId", (req, res) => {
+  const id = req.params.articleId;
+
+  const article = db.articles.find((article) => article.id === id);
+
+  if (!article) {
+    res.status(404).json({ error: "Article not found" });
+  } else {
+    res.json(article);
+  }
+});
+
+router.get("/articles/:articleId/comment", (req, res) => {
+  const id = req.params.articleId;
+
+  const comment = db.comments.find((comment) => comment.articleId === id);
+
+  if (!comment) {
+    res.status(404).json({ error: "Article not found" });
+  } else {
+    res.json(comment);
+  }
+});
+
+module.exports = router;

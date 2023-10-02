b0519242b42bc70106017bc69515fdc7c80421a0
sebtrn <sebastien.tran@edu.ece.fr>
2023-10-02 21:09:39 +0200

    feat: add routes
    defining deleteRoutes, getRoutes, postRoutes and putRoutes
    
    Update dall the files
    
    Ajusted and Updated my files program with corrections made by the Teacher
    
    lab3 - initializing routes
    
    initializing express routes

--- lab2/index.js
+++ lab2/index.js
@@ -1,7 +1,18 @@
-// Import a module
-const http = require('http')
-const handles = require('./handles')
+const express = require("express");
+const app = express();
+const db = require("./routes/database");
+const postRoutes = require("./routes/postRoutes");
+const putRoutes = require("./routes/putRoutes");
+const getRoutes = require("./routes/getRoutes");
+const deleteRoutes = require("./routes/deleteRoutes");
 
-http
-.createServer(handles.serverHandle)
-.listen(8080)
+app.set("port", 8080);
+
+app.use("/", getRoutes);
+app.use("/", postRoutes);
+app.use("/", putRoutes);
+app.use("/", deleteRoutes);
+
+app.listen(app.get("port"), () =>
+  console.log(`server listening on ${app.get("port")}`)
+);

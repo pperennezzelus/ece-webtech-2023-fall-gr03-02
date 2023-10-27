a0e402d71f77d716c070c64db746c259e7929768
sebtrn <77442193+sebtrn@users.noreply.github.com>
2023-10-28 00:18:26 +0200

    feat: adding profile API feature
    http://localhost:3000/api/profile

--- components/header.js
+++ components/header.js
@@ -1,37 +1,86 @@
-import React from 'react'
-import Link from 'next/link'
-
+import React, { useState, useEffect } from "react";
+import Link from "next/link";
 
 const Header = () => {
-    return (
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-        <header>
-            <nav className="bg-indigo-400 py-4">
-                <div className="container mx-auto flex justify-between items-center">
-                    <Link href="/" className="flex items-center text-2xl text-white font-semibold">
-                        <img src="logo.png" alt="Logo" className="h-8 w-8 mr-2" />
-                        Pinguin Motors
-                    </Link>
-                    <ul className="flex space-x-4">
-                        <li>
-                            <Link href="/" className="text-white hover:text-gray-300 transition duration-300">Home</Link>
-                        </li>
-                        <li>
-                            <Link href="/articles" className="text-white hover:text-gray-300 transition duration-300">Articles </Link>
-                        </li>
-                        <li>
-                            <Link href="/contact" className="text-white hover:text-gray-300 transition duration-300">Contact </Link>
-                        </li>
-                        <li>
-                            <Link href="/about" className="text-white hover:text-gray-300 transition duration-300">About </Link>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
+  useEffect(() => {
+    async function fetchProfile() {
+      try {
+        const response = await fetch("/api/profile");
+        if (response.status === 200) {
+          const data = await response.json();
+          setUser(data);
+        } else {
+          setUser(null);
+        }
+      } catch (error) {
+        console.error("Failed to fetch user profile:", error);
+      } finally {
+        setLoading(false);
+      }
+    }
 
+    fetchProfile();
+  }, []);
 
-        </header>
-    )
-}
+  return (
+    <header>
+      <nav className="bg-indigo-400 py-4">
+        <div className="container mx-auto flex justify-between items-center">
+          <Link
+            href="/"
+            className="flex items-center text-2xl text-white font-semibold"
+          >
+            <img src="logo.png" alt="Logo" className="h-8 w-8 mr-2" />
+            Pinguin Motors
+          </Link>
+          <ul className="flex space-x-4">
+            <li>
+              <Link
+                href="/"
+                className="text-white hover:text-gray-300 transition duration-300"
+              >
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/articles"
+                className="text-white hover:text-gray-300 transition duration-300"
+              >
+                Articles{" "}
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/contact"
+                className="text-white hover:text-gray-300 transition duration-300"
+              >
+                Contact{" "}
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/about"
+                className="text-white hover:text-gray-300 transition duration-300"
+              >
+                About{" "}
+              </Link>
+            </li>
+            {loading ? null : user ? (
+              <li className="flex items-center space-x-2">
+                {/* placeholder for an account icon.*/}
+                <span className="bg-white rounded-full p-2">🚹</span>
+                <span className="text-white">{user.username}</span>
+              </li>
+            ) : null}
+          </ul>
+        </div>
+      </nav>
+    </header>
+  );
+};
 
 export default Header;

35cb1bc7de586273044b57374f93f2599976fbe6
sebtrn <77442193+sebtrn@users.noreply.github.com>
2023-11-15 16:08:00 +0100

    feat: supabase next.js integration
    add admin/contact.js
    add .env
    update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,11 +2,20 @@ import "../styles/global.css";
 import RootLayout from "../components/layout";
 import { UserProvider } from "../components/UserContext";
 
+import { createPagesBrowserClient } from "@supabase/auth-helpers-nextjs";
+import { SessionContextProvider } from "@supabase/auth-helpers-react";
+import { useState } from "react";
 function MyApp({ Component, pageProps }) {
+  const [supabaseClient] = useState(() => createPagesBrowserClient());
   return (
     <UserProvider>
       <RootLayout>
-        <Component {...pageProps} />
+        <SessionContextProvider
+          supabaseClient={supabaseClient}
+          initialSession={pageProps.initialSession}
+        >
+          <Component {...pageProps} />
+        </SessionContextProvider>
       </RootLayout>
     </UserProvider>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,57 @@
-import React from 'react';
-import RootLayout from '../components/layout';
-import Link from 'next/link'
+import React from "react";
+import RootLayout from "../components/layout";
+import Link from "next/link";
 
 const Home = () => {
   return (
     <div className="container mx-auto mt-8">
-    <h1 className="text-4xl font-semibold text-center">Welcome to Pinguin Motors</h1>
-    <p className="text-lg text-gray-600 text-center mt-4">This is the website of group 2 in ING4 Gr3 <br></br> You can find us in the <a href='/contact'><strong>contact</strong></a> page!</p>
+      <h1 className="text-4xl font-semibold text-center">
+        Welcome to Pinguin Motors
+      </h1>
+      <p className="text-lg text-gray-600 text-center mt-4">
+        This is the website of group 2 in ING4 Gr3 <br></br> You can find us in
+        the{" "}
+        <a href="/contact">
+          <strong>contact</strong>
+        </a>{" "}
+        page!
+      </p>
 
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-8">
-        <Link href="/articles" className="bg-white p-4 rounded-lg shadow-lg block hover:bg-gray-100 transition duration-300">
-            <h2 className="text-xl font-semibold">Explore Our Inventory</h2>
-            <p className="mt-4">Discover a wide range of vehicles, from compact cars to luxury models.</p>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-8">
+        <Link
+          href="/articles"
+          className="bg-white p-4 rounded-lg shadow-lg block hover:bg-gray-100 transition duration-300"
+        >
+          <h2 className="text-xl font-semibold">Explore Our Inventory</h2>
+          <p className="mt-4">
+            Discover a wide range of vehicles, from compact cars to luxury
+            models.
+          </p>
         </Link>
 
-        <Link href="/contact" className="bg-white p-4 rounded-lg shadow-lg block hover:bg-gray-100 transition duration-300">
-            <h2 className="text-xl font-semibold">Discover Us</h2>
-            <p className="mt-4">Meet the passionate and creative minds behind Penguin Motors, dedicated to redefining your car-buying experience.</p>
+        <Link
+          href="/contact"
+          className="bg-white p-4 rounded-lg shadow-lg block hover:bg-gray-100 transition duration-300"
+        >
+          <h2 className="text-xl font-semibold">Discover Us</h2>
+          <p className="mt-4">
+            Meet the passionate and creative minds behind Penguin Motors,
+            dedicated to redefining your car-buying experience.
+          </p>
         </Link>
 
-        <Link href="/about" className="bg-white p-4 rounded-lg shadow-lg block hover:bg-gray-100 transition duration-300">
-            <h2 className="text-xl font-semibold">Learn More</h2>
-            <p className="mt-4">Curious to know more about the vision and values that drive Penguin Motors? Learn about us and our journey.</p>
+        <Link
+          href="/about"
+          className="bg-white p-4 rounded-lg shadow-lg block hover:bg-gray-100 transition duration-300"
+        >
+          <h2 className="text-xl font-semibold">Learn More</h2>
+          <p className="mt-4">
+            Curious to know more about the vision and values that drive Penguin
+            Motors? Learn about us and our journey.
+          </p>
         </Link>
-      
+      </div>
     </div>
-</div>
-
-  )
-}
+  );
+};
 export default Home;

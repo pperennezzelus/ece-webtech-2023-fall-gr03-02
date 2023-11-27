4879b026e4e871238c5c89bc53e68eaa0714504b
pperennezzelus <99469951+pperennezzelus@users.noreply.github.com>
2023-11-27 20:37:26 +0100

    feat: redirect user when sign up

--- pages/login.js
+++ pages/login.js
@@ -22,6 +22,7 @@ const LoginPage = () => {
         supabaseClient={supabase}
         appearance={{ theme: ThemeSupa }}
         providers={["github"]}
+        redirectTo="https://ece-webtech-2023-fall-gr03-02.vercel.app/login"
       />
     </div>
   );
